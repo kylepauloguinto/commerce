e4f7f3e5c6d45cfb5b3fbba786f2162b72a2befb
guintokylep <guintokyle01@gmail.com>
2025-10-27 23:55:15 +0900

    try fix

--- commerce/settings.py
+++ commerce/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-# # Optional: if you have additional static folders
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'auctions/static'),
-# ]
+# Optional: if you have additional static folders
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
