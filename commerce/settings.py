b903fc6ba6f207be652a7ec49b3e680de7cffc15
guintokylep <guintokyle01@gmail.com>
2025-10-27 22:56:18 +0900

    added version control, and notch fixed bug for ios

--- commerce/settings.py
+++ commerce/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Optional: if you have additional static folders
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'auctions/static'),
